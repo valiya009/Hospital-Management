3f953c7ea24749a978bacb58f09ed6fbaad5010f
valiya009 <valiyachandresh33@gmail.com>
2025-05-01 01:24:07 -0700

    update messages and api

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,6 +1,5 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
-  // Determine the cookie name based on the user's role
   const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
 
   res
